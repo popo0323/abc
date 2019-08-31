64ad39f9cabac9326c380e9cce0ef0a90dff7b6d
popo0323 <popo0323@users.noreply.github.com>
2019-08-31 19:00:47 +0900

    Update 138_e.py
    try分岐が動かなかった、-1出力のexit忘れてた修正

--- 138_e.py
+++ 138_e.py
@@ -1,3 +1,5 @@
+import sys
+
 s = str(input())
 t = str(input())
 
@@ -6,23 +8,25 @@ tt = tuple(t)
 sg = set(st)
 tg = set(tt)
 
-if not tg & sg:
+if not tg <= sg:
   print('-1')
+  sys.exit()
 
 cnt = 0
 f = int(len(st)/2)
 sta = st
 
 for i in tt:
-    try:
-      f = sta.index(i,f+1,len(sta))
-      print(f)
-    except ValueError:
-      f = sta.index(i,0,f+1)
-      cnt += 1
-      sta += sta
-      print(cnt)
-      print(f)
-      print(sta)
-
-print(len(st) * cnt + f + 1)
+  try:
+    f = sta.index(i,f+1,len(sta))
+    a = True
+  except ValueError:
+    f = sta.index(i,0,f+1)
+    cnt += 1
+    sta += sta
+    print(cnt)
+    print(f)
+    print(sta)
+    a = False
+      
+print(len(st) * (cnt - a) + f + 1)
