8b8f70e5d555e12a18ef0a94b9fec27d353307c2
popo0323 <popo0323@users.noreply.github.com>
2019-08-31 18:41:38 +0900

    Update 138_e.py
    RE 同じ文字を続ける入力に対応

--- 138_e.py
+++ 138_e.py
@@ -9,38 +9,20 @@ tg = set(tt)
 if not tg & sg:
   print('-1')
 
-sb = st[int(len(st)/2):]
-sf = st[:int(len(st)/2)]
 cnt = 0
 f = int(len(st)/2)
 sta = st
 
 for i in tt:
     try:
+      f = sta.index(i,f+1,len(sta))
       print(f)
-      f = sta.index(i,f,len(st))
     except ValueError:
-      f = sta.index(i,0,f)
+      f = sta.index(i,0,f+1)
       cnt += 1
       sta += sta
+      print(cnt)
+      print(f)
+      print(sta)
 
 print(len(st) * cnt + f + 1)
-
-
-print('----------------------')
-print(s[:])
-
-print(s in t)
-print(st)
-print(tt)
-print(st in tt)
-
-print(sg)
-print(tg)
-
-print(tg in sg)
-print(tg & sg == True)
-print(4 == True)
-
-if tg & sg:
-  print('True!')
